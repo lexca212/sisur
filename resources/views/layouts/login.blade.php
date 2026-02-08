c9a442b1552d7bcc7940dd322e8714d26bdaba5f
lexca212 <lexca@DESKTOP-JCOH9OA.localdomain>
2026-02-08 22:31:10 +0700

    Menambah hal login

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Dashboard')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+
+    <!-- AdminLTE CSS -->
+    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+
+    @stack('css')
+</head>
+<body class="">
+
+<div class="">
+
+    {{-- Navbar
+    @include('layouts.partials.navbar') --}}
+
+    {{-- Sidebar
+    @include('layouts.partials.sidebar') --}}
+
+    {{-- Content --}}
+    <div class="content p-5">
+        @yield('content')
+    </div>
+
+    {{-- Footer
+    @include('layouts.partials.footer')  --}}
+
+</div>
+
+<!-- jQuery -->
+<script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+
+<!-- Bootstrap 4 -->
+<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- AdminLTE JS -->
+<script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+
+@stack('js')
+
+</body>
+</html>
